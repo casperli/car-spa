4f526a536171fc0eac97c53852cdc0783170d880
Andreas Caspers <aca@zuehlke.com>
2016-03-26 11:18:51 +0100

    start completed

---
+++ client/app/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var carSpa = angular.module('carSpa', []);
+
+carSpa.controller('CarListController', CarListController);
+
+function CarListController($scope) {
+    var vm = this;
+
+    vm.isInitialized = false;
+    vm.showWelcome = true;
+
+    function startWork() {
+        vm.showWelcome = false;
+    }
+
+    function init() {
+        setTimeout(function () {
+            vm.isInitialized = true;
+            // We need this, cause no watched expression is triggered by the setTimeout function
+            $scope.$apply();
+        }, 2000);
+    }
+
+    vm.startWork = startWork;
+
+    init();
+
+    return vm;
+}
